2a26f73858abfd29f0ae72778f745e60ddd7a803
Amber Medina <alouiseme@gmail.com>
2019-05-14 14:54:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function logShout(string){
+    console.log.toUpperCase()
+}
+
+function logWhisper(string){
+
+}
+
+function sayHiToGrandma(string){
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+if (string === string.toLowerCase()){
+return ("I can't hear you!")}}
+
+function sayHiToGrandma(string) {
+if (string === string.toUpperCase());
+return ("YES INDEED!");
+}
+
+function sayHiToGrandma(string) {
+if (string === "I love you, Grandma.")
+return ('I love you, too.')
+}
